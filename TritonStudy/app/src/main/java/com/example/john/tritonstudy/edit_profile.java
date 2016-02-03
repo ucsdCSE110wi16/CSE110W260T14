0401ad7392b6536c7391504ea4201716dfe11b75
AbhiSwag <abs012@ucsd.edu>
2016-02-02 17:57:24 -0800

    Changed picture name

--- TritonStudy/app/src/main/java/com/example/john/tritonstudy/edit_profile.java
+++ TritonStudy/app/src/main/java/com/example/john/tritonstudy/edit_profile.java
@@ -14,7 +14,7 @@ public class edit_profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile
+        setContentView(R.layout.activity_edit_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
