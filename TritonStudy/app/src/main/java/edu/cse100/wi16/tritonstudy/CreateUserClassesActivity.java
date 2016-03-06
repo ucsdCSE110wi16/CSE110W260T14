e3ce42c0b0ed6fde14706afad3e07d5d67af5561
John Rodgers <jyrodger@ucsd.edu>
2016-03-05 23:18:06 -0800

    Revamped Student, User Creation
    - Student object is now Parsable.
    - Student object now includes 4 string to hold class names
    - Information entered in CreateUserInfo activity is passed in a bundle
    to CreateUserClasses.
    - Disclaimer: Account is created but info is not saved in database. I’m
    getting an error “Permission Denied”

--- TritonStudy/app/src/main/java/edu/cse100/wi16/tritonstudy/CreateUserClassesActivity.java
+++ TritonStudy/app/src/main/java/edu/cse100/wi16/tritonstudy/CreateUserClassesActivity.java
@@ -3,25 +3,49 @@ package edu.cse100.wi16.tritonstudy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateUserClassesActivity extends AppCompatActivity {
+
+
+    Student student;
+    Firebase ref = new Firebase("https://sweltering-inferno-5625.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //receive
+        student = (Student)getIntent().getParcelableExtra(CreateUserInfo.PAR_KEY);
+
+        Log.d("TEST", student.getName());
+
+
+
+
         setContentView(R.layout.activity_create_user_classes);
 
-        //Next Button
-        Button nextButton = (Button) findViewById(R.id.buttonNext);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(CreateUserClassesActivity.this, CreateUserClassesConfirmActivity.class));
-            }
-        });
+//        //Next Button
+//        Button nextButton = (Button) findViewById(R.id.buttonNext);
+//        nextButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//
+//
+//                startActivity(new Intent(CreateUserClassesActivity.this, CreateUserClassesConfirmActivity.class));
+//            }
+//        });
 
         //Back Button
         Button backButton = (Button) findViewById(R.id.buttonBack);
@@ -62,5 +86,76 @@ public class CreateUserClassesActivity extends AppCompatActivity {
 
             }
         });
+
+
+    }
+
+    public void next(View v){
+
+        Spinner spClass1 =(Spinner) findViewById(R.id.createUser_spinner_class1);
+        student.setClass1(spClass1.getSelectedItem().toString());
+        Log.d("Student Class 1", student.getClass1());
+
+        Spinner spClass2 =(Spinner) findViewById(R.id.createUser_spinner_class2);
+        student.setClass2(spClass2.getSelectedItem().toString());
+
+        Spinner spClass3 =(Spinner) findViewById(R.id.createUser_spinner_class3);
+        student.setClass3(spClass3.getSelectedItem().toString());
+
+        Spinner spClass4 =(Spinner) findViewById(R.id.createUser_spinner_class4);
+        student.setClass4(spClass4.getSelectedItem().toString());
+
+
+        Log.d("Student Class 2", student.getClass2());
+        Log.d("Student Class 3", student.getClass3());
+        Log.d("Student Class 4", student.getClass4());
+
+        String email = student.getEmail();
+        String password = student.getPassword();
+
+        ref.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+
+                String uid = result.get("uid").toString();
+
+                Log.d("ref", ref.toString());
+                Log.d("users", ref.child("users").toString());
+                Log.d("uid", ref.child("users").child(uid).toString());
+
+//                Map<String, Object> map = new Map<String, Object>();
+                HashMap<String, Object> map = new HashMap<String, Object>();
+
+                map.put("name", student.getName());
+                map.put("email", student.getEmail());
+                map.put("password", student.getPassword());
+                map.put("major", student.getMajor());
+                map.put("bio", student.getBio());
+                map.put("class1", student.getClass1());
+                map.put("class2", student.getClass2());
+                map.put("class3", student.getClass3());
+                map.put("class4", student.getClass4());
+
+
+
+                ref.child("users").child(uid).setValue(map);
+
+//                ref.child("users/"+student.getName()).setValue(student);
+
+                System.out.println("Successfully created user account for " + student.getName());
+                Toast.makeText(CreateUserClassesActivity.this, "Successfully created user account for " + student.getName(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                Toast.makeText(CreateUserClassesActivity.this, "Shit is Broke!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        startActivity(new Intent(CreateUserClassesActivity.this, LoginActivity.class));
+
+
+
     }
 }
