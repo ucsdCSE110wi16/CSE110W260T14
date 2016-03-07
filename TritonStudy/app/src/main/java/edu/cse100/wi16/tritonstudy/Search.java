5b8079cc3270a085d22c9a71b33ff0bfed3f53a0
John Rodgers <jyrodger@ucsd.edu>
2016-03-07 13:35:39 -0800

    Search: Puts results in hash map and sends to another activity
    - Still a work in progress.
    - Choose a class in search. The DB is looped through to find all users
    with that class, they are put in a map that is passed to a test
    activity.
    - That test activity receives the map and loops through it printing all
    of the values.

--- TritonStudy/app/src/main/java/edu/cse100/wi16/tritonstudy/Search.java
+++ TritonStudy/app/src/main/java/edu/cse100/wi16/tritonstudy/Search.java
@@ -1,5 +1,6 @@
 package edu.cse100.wi16.tritonstudy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +13,23 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 
 public class Search extends AppCompatActivity {
 
     Firebase rootRef = new Firebase("https://sweltering-inferno-5625.firebaseio.com/");
+//    private Vector searchResults;
+//    List<String> searchResults;
+Hashtable<String, String> searchResults = new Hashtable<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +40,13 @@ public class Search extends AppCompatActivity {
 
         setSpinnerValues();
 
+
     }
 
     private void setSpinnerValues() {
 
         Spinner spinner = (Spinner) findViewById(R.id.search_spinner);
-        String[] arrayCSEcourses = new String[]{"Choose Course", "CSE 3", "CSE 7", "CSE 8A", "CSE 8B", "CSE 11",
+        String[] arrayCSEcourses = new String[]{"CSE 3", "CSE 7", "CSE 8A", "CSE 8B", "CSE 11",
                 "CSE 12", "CSE 15L", "CSE 20", "CSE 21", "CSE 30", "CSE 100", "CSE 101", "CSE 105",
                 "CSE 110", "CSE 120", "CSE 127", "CSE 130", "CSE 131", "CSE 136", "CSE 140",
                 "CSE 140L", "CSE 141", "CSE 141L", "CSE 148", "CSE 150", "CSE 152",
@@ -44,35 +58,23 @@ public class Search extends AppCompatActivity {
 
     private String getSpinnerValues() {
         Spinner spinner = (Spinner) findViewById(R.id.search_spinner);
-        String classToFind = spinner.getSelectedItem().toString();
-        return classToFind;
+        return (spinner.getSelectedItem().toString());
+
     }
 
     public void onSearchButtonClick(View v) {
-
-        final String classTofind = getSpinnerValues();
-
+        Hashtable<String, String> results = new Hashtable<String, String>();
         Firebase usersRef = rootRef.child("users/");
-
-
-        // Attach an listener to read the data at our posts reference
-        usersRef.addValueEventListener(new ValueEventListener() {
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
 
-                Log.d("Debug", "//////////////////////////");
-                Log.d("DEBUG", "The class to find is " + classTofind);
-                Log.d("Debug", "There are " + snapshot.getChildrenCount() + " children");
+                String classTofind = getSpinnerValues();
+                HashMap<String, String> results = new HashMap<String, String>();
 
-                Vector searchResults = new Vector(1, 1);
 
                 for (DataSnapshot child : snapshot.getChildren()) {
                     Student student = child.getValue(Student.class);
-
-                    String student1Ref = child.getRef().toString();
-                    refToStudentTest(student1Ref);
-
-
 
                     if (student.getClass1().equals(classTofind) ||
                             student.getClass2().equals(classTofind) ||
@@ -80,60 +82,103 @@ public class Search extends AppCompatActivity {
                             student.getClass4().equals(classTofind)) {
 
                         Log.d("Debug", "The Student's name is " + student.getName());
-                        searchResults.add(student.getName());
-
+                        String studentRef = child.getRef().toString();
+                        results.put(student.getName(), studentRef);
                     }
                 }
 
-                for (Object obj : searchResults) {
-                    if (obj instanceof String) {
-                        Log.d("Debug", "Search Result: " + obj);
-                    }
-                }
+                Log.d("Debug", "get UID from hashmap" + results.get("Antelope"));
+//                Bundle bundle = new Bundle();
+//                bundle.putSerializable("results", results);
+//
+//                Intent intent = new Intent(Search.this, testReceiveResults.class);
+//                intent.putExtras(bundle);
+//                startActivity(intent);
 
+                startActivity(new Intent(Search.this, testReceiveResults.class).putExtra("hashMapKey", results));
             }
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-                System.out.println("The read failed: " + firebaseError.getMessage());
             }
         });
-
     }
 
-    private void refToStudentTest(final String student1RefLocation){
+    private void addToSearchResults(Hashtable<String,String> results){
+        this.searchResults = results;
+    }
 
-        final Firebase student1Ref = new Firebase(student1RefLocation);
+//    private Hashtable<String, String> getSearchResults(){
+//
+//
+//        return results;
+//
+//    }
+
+    private void findSearchResults(){
 
         Firebase usersRef = rootRef.child("users/");
-
-
-        // Attach an listener to read the data at our posts reference
-        usersRef.addValueEventListener(new ValueEventListener() {
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
 
-                Vector searchResults = new Vector(1, 1);
+                String classTofind = getSpinnerValues();
+                Hashtable<String, String> results = new Hashtable<String, String>();
 
                 for (DataSnapshot child : snapshot.getChildren()) {
+                    Student student = child.getValue(Student.class);
 
-                    if (child.getRef().equals(student1Ref)){
+                    if (student.getClass1().equals(classTofind) ||
+                            student.getClass2().equals(classTofind) ||
+                            student.getClass3().equals(classTofind) ||
+                            student.getClass4().equals(classTofind)) {
 
-                        Student student2 = child.getValue(Student.class);
-                        Log.d("DEBUG", "Student2.getName() = " + student2.getName());
+                        Log.d("Debug", "The Student's name is " + student.getName());
+                        String studentRef = child.getRef().toString();
+                        results.put(student.getName(), studentRef);
                     }
-
                 }
 
             }
-
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-                System.out.println("The read failed: " + firebaseError.getMessage());
             }
         });
-
-
-
     }
+
+//    private void refToStudentTest(final String student1RefLocation){
+//
+//        final Firebase student1Ref = new Firebase(student1RefLocation);
+//
+//        Firebase usersRef = rootRef.child("users/");
+//
+//
+//        // Attach an listener to read the data at our posts reference
+//        usersRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot snapshot) {
+//
+//                Vector searchResults = new Vector(1, 1);
+//
+//                for (DataSnapshot child : snapshot.getChildren()) {
+//
+//                    if (child.getRef().equals(student1Ref)){
+//
+//                        Student student2 = child.getValue(Student.class);
+//                        Log.d("DEBUG", "Student2.getName() = " + student2.getName());
+//                    }
+//
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(FirebaseError firebaseError) {
+//                System.out.println("The read failed: " + firebaseError.getMessage());
+//            }
+//        });
+//
+//
+//
+//    }
 }
